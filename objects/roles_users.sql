643ef9c634101e9a7de6922c3fbf47f212fcf5a7
Marjean17 <95670933+Marjean17@users.noreply.github.com>
2024-05-15 17:06:15 +0000

    se continua trabajando sobre los roles

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -5,12 +5,12 @@ CREATE USER IF NOT EXISTS 'admin'@'%' INDENTIFIED BY '123';
 
 --GRANT ALL PRIVILEGES
 GRANT ALL PRIVILEGES ON ferreteria_jeanneret.* TO 'admin'@'%' WHIT GRANT OPTION;
-
 FLUSH PRIVILEGES;
 
 USE ferreteria_jeanneret;
 
--- CREACI0N DE ROLES Y ASGNACION DE PRIVILEGIOS
+
+-- CREACION DE ROLES Y ASGNACION DE PRIVILEGIOS
 CREATE ROLE role_compras_caba;
 
 GRANT SELECT ON ferreteria_jeanneret.proveedores 
@@ -33,9 +33,22 @@ WHERE DIREC_PROV NOT LIKE '%CABA' TO role_compras_interior;
 
 CREATE ROLE role_ventas;
 
-GRANT SELECT ON ferreteria_jeanneret.clientes 
+GRANT SELECT ON ferreteria_jeanneret.clientes, stock 
 GRANT UPDATE, DELETE ON 
       ferreteria_jeanneret.clientes
-    , ferreteria_jeanneret.factura_compra
-    , ferreteria_jeanneret.detalle_compra 
-WHERE DIREC_PROV NOT LIKE '%CABA' TO role_compras_ventas;
+    , ferreteria_jeanneret.factura_venta
+    , ferreteria_jeanneret.detalle_venta
+TO role_ventas;
+
+-- CREACION DE USUARIOS Y ASIGNACION DE ROLES
+
+CREATE USER IF NOT EXISTS 'juan'@'%' INDENTIFIED BY '111' COMMENT 'Equipo de Compras - CABA';
+GRANT role_compras_caba TO 'juan'@'%';
+
+CREATE USER IF NOT EXISTS 'Pedro'@'%' INDENTIFIED BY '222' COMMENT 'Equipo de Compras - Interior';
+GRANT role_compras_interior TO 'Pedro'@'%';
+
+CREATE USER IF NOT EXISTS 'Maria'@'%' INDENTIFIED BY '333' COMMENT 'Equipo de Ventas';
+GRANT role_compras_interior TO 'Maria'@'%';
+
+FLUSH PRIVILEGES;
