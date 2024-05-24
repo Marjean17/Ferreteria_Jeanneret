2ce0cf3116e92787195a52ff4e8c4be2a5eab500
Marjean17 <95670933+Marjean17@users.noreply.github.com>
2024-05-24 22:05:09 +0000

    se sueben los roles

--- objects/roles_users.sql
+++ objects/roles_users.sql
@@ -1,54 +1,58 @@
 use mysql;
 
---CREATE USER IF NOT EXISTS
-CREATE USER IF NOT EXISTS 'admin'@'%' INDENTIFIED BY '123';
-
---GRANT ALL PRIVILEGES
-GRANT ALL PRIVILEGES ON ferreteria_jeanneret.* TO 'admin'@'%' WHIT GRANT OPTION;
+-- Creo un usuario Admin y le doy todos los privilegios
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON ferreteria3hermanos.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
 
-USE ferreteria_jeanneret;
-
-
 -- CREACION DE ROLES Y ASGNACION DE PRIVILEGIOS
-CREATE ROLE role_compras_caba;
+CREATE ROLE IF NOT EXISTS role_compras_caba;
+GRANT SELECT ON ferreteria3hermanos.proveedores TO role_compras_caba;
+GRANT SELECT ON ferreteria3hermanos.descripcion_productos TO role_compras_caba;
+GRANT SELECT ON ferreteria3hermanos.stock TO role_compras_caba;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.proveedorescaba TO role_compras_caba;
+GRANT SELECT, INSERT ON ferreteria3hermanos.factura_compra TO role_compras_caba;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.detalle_compra TO role_compras_caba;
 
-GRANT SELECT ON ferreteria_jeanneret.proveedores 
-GRANT UPDATE, DELETE ON 
-      ferreteria_jeanneret.proveedores
-    , ferreteria_jeanneret.factura_compra
-    , ferreteria_jeanneret.detalle_compra 
-WHERE DIREC_PROV LIKE '%CABA' TO role_compras_caba;
+CREATE ROLE IF NOT EXISTS role_compras_interior;
+GRANT SELECT ON ferreteria3hermanos.proveedores TO role_compras_interior;
+GRANT SELECT ON ferreteria3hermanos.descripcion_productos TO role_compras_interior;
+GRANT SELECT ON ferreteria3hermanos.stock TO role_compras_interior;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.proveedoresinterior TO role_compras_interior;
+GRANT SELECT, INSERT ON ferreteria3hermanos.factura_compra TO role_compras_interior;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.detalle_compra TO role_compras_interior;
+
+CREATE ROLE IF NOT EXISTS role_ventas;
+GRANT SELECT ON ferreteria3hermanos.clientes TO role_ventas;
+GRANT SELECT ON ferreteria3hermanos.descripcion_productos TO role_ventas;
+GRANT SELECT ON ferreteria3hermanos.stock TO role_ventas;
+GRANT SELECT, INSERT ON ferreteria3hermanos.factura_venta TO role_ventas;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.detalle_venta TO role_ventas;
 
 
-CREATE ROLE role_compras_interior;
+CREATE ROLE IF NOT EXISTS role_jefe_compras;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.proveedores TO role_jefe_compras;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.stock TO role_jefe_compras;
 
-GRANT SELECT ON ferreteria_jeanneret.proveedores 
-GRANT UPDATE, DELETE ON 
-      ferreteria_jeanneret.proveedores
-    , ferreteria_jeanneret.factura_compra
-    , ferreteria_jeanneret.detalle_compra 
-WHERE DIREC_PROV NOT LIKE '%CABA' TO role_compras_interior;
-
-
-CREATE ROLE role_ventas;
-
-GRANT SELECT ON ferreteria_jeanneret.clientes, stock 
-GRANT UPDATE, DELETE ON 
-      ferreteria_jeanneret.clientes
-    , ferreteria_jeanneret.factura_venta
-    , ferreteria_jeanneret.detalle_venta
-TO role_ventas;
+CREATE ROLE IF NOT EXISTS role_jefe_ventas;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.clientes TO role_jefe_ventas;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ferreteria3hermanos.stock TO role_jefe_ventas;
 
 -- CREACION DE USUARIOS Y ASIGNACION DE ROLES
 
-CREATE USER IF NOT EXISTS 'juan'@'%' INDENTIFIED BY '111' COMMENT 'Equipo de Compras - CABA';
+CREATE USER IF NOT EXISTS 'juan'@'%' IDENTIFIED BY '111' COMMENT "Equipo de Compras - CABA";
 GRANT role_compras_caba TO 'juan'@'%';
 
-CREATE USER IF NOT EXISTS 'Pedro'@'%' INDENTIFIED BY '222' COMMENT 'Equipo de Compras - Interior';
+CREATE USER IF NOT EXISTS 'Pedro'@'%' IDENTIFIED BY '222' COMMENT "Equipo de Compras - Interior";
 GRANT role_compras_interior TO 'Pedro'@'%';
 
-CREATE USER IF NOT EXISTS 'Maria'@'%' INDENTIFIED BY '333' COMMENT 'Equipo de Ventas';
-GRANT role_compras_interior TO 'Maria'@'%';
+CREATE USER IF NOT EXISTS 'Maria'@'%' IDENTIFIED BY '333' COMMENT "Equipo de Ventas";
+GRANT role_ventas TO 'Maria'@'%';
+
+CREATE USER IF NOT EXISTS 'Lucas'@'%' IDENTIFIED BY '111' COMMENT "Jefe - Equipo de Compras";
+GRANT role_compras_caba, role_compras_interior, role_jefe_compras TO 'Lucas'@'%';
+
+CREATE USER IF NOT EXISTS 'Marcos'@'%' IDENTIFIED BY '111' COMMENT "Jefe - Equipo de Ventas";
+GRANT role_ventas, role_jefe_ventas TO 'Marcos'@'%';
 
 FLUSH PRIVILEGES;
